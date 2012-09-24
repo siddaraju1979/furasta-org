4006cc35501f117e1fc2fdf0bb1cc6af33f3756d
conormacaoidh@gmail.com <conormacaoidh@gmail.com@6abc49e9-9742-383f-1118-7ab55080966b>
2012-09-24 20:30:39 +0000

    some ui updates to the file manager.

--- trunk/admin/files.php
+++ trunk/admin/files.php
@@ -22,9 +22,11 @@ $content = '
 <div id="file-manager">
         <div id="files">
                 <div id="directory-container">
-                        <div class="th"><p style="color:#fff">Directory Listing</p></div>
+                        <div class="th"><h3 style="color:#fff;margin-top:5px;margin-left:5px">Directory Listing</h3></div>
                         <ul id="panel-top">
-                                <li><a class="link" id="new-folder">New Folder</a></li>
+                                <li><a class="link" id="file-upload"><img src="' . SITE_URL . '_inc/img/file.png"/><span>Upload File</span></a></li>
+                                <li><a class="link" id="new-folder"><img src="' . SITE_URL . '_inc/img/folder.png"/><span>New Folder</span></a></li>
+                                <li><a class="link" id="dir-settings"><span id="menu-configuration-img"></span><span>Folder Settings</span></a></li>
                         </ul>
                         <div id="filecrumbs"></div>
                         <ul id="directory-list"></ul>      
